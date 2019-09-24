0135828070009bade6779de387f4099affd24084
Fiona <fxzhou@ucdavis.edu>
2019-09-23 21:27:47 -0700

    changed 'put' to 'post'

--- controllers/burger-controller.js
+++ controllers/burger-controller.js
@@ -28,7 +28,7 @@ router.post("/api/burgers", function(req, res) {
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.post("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   burger.update({ devoured: true }, condition, function(result) {
